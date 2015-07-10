7171f094fa362734bb06561ca6cb9ca4d7e09e94
Krista Jackson <krista.lynn.jackson@gmail.com>
2015-07-10 13:30:07 -0700

    Add ::columns to lib/01_sql_object.rb file.

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -3,7 +3,16 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    # gets the columns from the table and symbolizes them
+
+    col_and_data = DBConnection.execute2(<<-SQL)
+                      SELECT
+                        *
+                      FROM
+                        #{table_name}
+                    SQL
+
+    col_and_data.first.collect! { |col_name_str| col_name_str.to_sym }
   end
 
   def self.finalize!
@@ -16,8 +25,8 @@ class SQLObject
 
   def self.table_name
     # gets the name of the table for the class
-    @table_name || self.name.tableize
     # both .name or .to_s would work here
+    @table_name || self.name.tableize
   end
 
   def self.all
